d461c01dbb909af53bee8d22594858fb7fe6fdc7
Israel Theodros Seleshi <123582019+israelseleshi@users.noreply.github.com>
2024-07-27 23:49:07 +0300

    Math Object

---
+++ P08_Math_object/index.js
@@ -0,0 +1,39 @@
+// Math = built in object that provides a collection of properties and methods.
+
+// PROPERTIES (Math.PI & Math.E)
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let x = 3.99;
+let y = -2;
+let a = 45;
+let z;
+
+// METHODS
+// z = Math.round(x);
+// z = Math.floor(x); // output: 3
+// z = Math.ceil(x); // output: 4
+// z = Math.trunc(x); // Math.trunc() method truncates any decimals from a number.
+// z = Math.pow(3, 2);
+// z = Math.sqrt(81);
+// z = Math.log(x);
+
+// Methods related to Trigonometry
+// z = Math.sin(a);
+// z = Math.cos(a);
+// z = Math.tan(a);
+// z = Math.abs(y);
+// z = Math.sign(y); // returns the sign of a number as -1 for negatives and 1 for positives.
+// console.log(z);
+
+
+// Methods to find maximum and minimum
+// min and max functions
+let d = 3;
+let e = 2;
+let f = 1;
+
+let max = Math.max(d, e, f);
+let min = Math.min(d, e, f);
+console.log(max);
+console.log(min);
