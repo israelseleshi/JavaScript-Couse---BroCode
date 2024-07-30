f60aee5079e6fb9e3efcc60de778548ab1702bb9
Israel Theodros Seleshi <123582019+israelseleshi@users.noreply.github.com>
2024-07-30 19:24:07 +0300

    For Loops

---
+++ P20_For_Loops/index.js
@@ -0,0 +1,15 @@
+// for loop = repeat some code a LIMITED amount of times.
+
+// for(let i = 10; i > 0; i -= 3) {
+//   console.log(i);
+// }
+// console.log("HAPPY NEW YEAR!");
+
+for(let i = 1; i <= 20; i++) {
+  if(i == 13) {
+    break;
+  }
+  else {
+    console.log(i);
+  }
+}
